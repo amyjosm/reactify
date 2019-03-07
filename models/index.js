04fce184e76bd9628fd408b5c5759e7228b2f028
tuckerbeauchamp <tuckerbeauchamp@gmail.com>
2019-03-07 14:51:36 -0700

    commiting for bbackup

--- models/index.js
+++ models/index.js
@@ -1,3 +1,21 @@
-module.exports = {
-  User: require("./user")
-};
+// module.exports = {
+//   User: require("./user")
+// };
+
+const fs = require("fs");
+const path = require("path");
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || "development";
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter(
+    file =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach(file => {
+    const model = require("./" + file);
+    db[model.modelName] = model;
+  });
+
+module.exports = db;
--- server.js
+++ server.js
@@ -4,24 +4,36 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Passport configuration
-// const passport = require("passport");
+// Passport Configuration
 require("./services/passport");
 
 // Mongoose Connection
-const db = require("./config/connection");
-db(process.env.MONGODB_URI || "mongodb://localhost/test");
+const connection = require("./confing/connection");
+let mongoUrl;
+
+if (process.env.MONGODB_URI) {
+  mongoUrl = process.env.MONGODB_URI;
+} else if (env === "test") {
+  mongoUrl = "mongodb://localhost/test";
+} else {
+  mongoUrl = "mongodb://localhost/reactify";
+}
 
 // Express Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require("morgan")("dev"));
 
-// Serve Static
+// Serve Static Assets
 app.use(express.static("app/build"));
 
+// Add Router Middleware
 app.use(require("./routes"));
 
-app.listen(PORT, function() {
-  console.log(`App listening on PORT ${PORT}`); // es
+connection(mongoUrl).then(function(){
+  app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+  });
 });
+
+module.exports = app;
